248a5326ebdd4142f81c0509f3e70d158ee19fee
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-03-06 12:17:04 +0000

    Traffic Light React Jonay

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [lightGreen,setLightGreen] = useState("btn-success")
+	const [lightYellow,setLightYellow] = useState("btn-warning")
+	const [lightRed,setLightRed] = useState("btn-danger")
+
+	function changeColor() {
+		if (lightRed === "btn-danger") {
+			setLightRed("btn-secondary")
+		} else {
+			setLightRed("btn-danger")
+		}
+		
+	// if (lightGreen === "btn-success") {
+	// 	setLightYellow("btn-secondary")
+	// 	setLightRed("btn-secondary")
+	// }
+	// 	setLightGreen("btn-secondary")
+	// 	setLightYellow("btn-secondary")
+	// 	setLightRed("btn-secondary")
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+		<div className="traffic top bg-dark d-flex">top</div>
+			<div className="container d-flex align-items-center flex-column bg-dark">
+				<div className={"btn rounded-circle "+lightRed} onClick={changeColor}>red</div>
+				<div className={"btn rounded-circle "+lightYellow} onClick={changeColor}>yellow</div>
+				<div className={"btn rounded-circle "+lightGreen} onClick={changeColor}>green</div>
+			</div>
+		</>
+
+		// <div className="text-center">
+		// 	<h1 className="text-center mt-5">Seguidores {contador}</h1>
+		// 	<p>
+		// 		<img src={rigoImage} />
+		// 	</p>
+		// 	<a href="#" className={"btn "+colorBtn} onClick={contadorFollowers}>
+		// 		{text}
+		// 	</a>
+		// 	<p>
+		// 		Made by{" "}
+		// 		<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+		// 		love!
+		// 	</p>
+		// </div>
 	);
 };
 
