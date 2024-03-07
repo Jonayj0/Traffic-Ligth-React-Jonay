6fb3afb99be0db457b7e48f1b224acbb29ad9687
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-03-07 12:06:36 +0000

    Traffic Light React Jonay con Boton

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 	const [lightYellow,setLightYellow] = useState("btn-secondary")
 	const [lightRed,setLightRed] = useState("btn-secondary")
 
-	function changeColor() {
+	function modSemaforo() {
 		if (lightRed === "btn-danger") {
 			setLightRed("btn-secondary")
 			setLightGreen("btn-success")
@@ -17,7 +17,7 @@ const Home = () => {
 		} else if (lightYellow === "btn-warning"){
 			setLightYellow("btn-secondary")
 			setLightRed("btn-danger")
-		} else lightGreen === "btn-success"
+		} else setLightGreen("btn-success")
 	}
 
 	return (
@@ -25,12 +25,39 @@ const Home = () => {
 		<div className={"container d-flex justify-content-center bg-dark text-white"}>
 		</div>
 			<div className={"ligths container d-flex justify-content-center flex-column bg-dark"}>
-				<div className={"btn rounded-circle "+lightRed} onClick={changeColor}></div>
-				<div className={"btn rounded-circle "+lightYellow} onClick={changeColor}></div>
-				<div className={"btn rounded-circle "+lightGreen} onClick={changeColor}></div>
+				<div className={"btn rounded-circle "+lightRed} onClick={changeColorRed}></div>
+				<div className={"btn rounded-circle "+lightYellow} onClick={changeColorYellow}></div>
+				<div className={"btn rounded-circle "+lightGreen} onClick={changeColorGreen}></div>
 			</div>
+			<div className="d-flex justify-content-center">
+			<div className="btn btn-info mt-5" onClick={modSemaforo}><b>SEMAFORO</b></div>
+		</div>
 		</>
 	);
+
+	function changeColorRed() {
+				if (lightRed === "btn-danger") {
+					setLightRed("btn-secondary")
+					} else setLightRed("btn-danger")
+					setLightYellow("btn-secondary")
+					setLightGreen("btn-secondary")
+				}
+	
+	function changeColorYellow() {
+				if (lightYellow === "btn-warning") {
+					setLightYellow("btn-secondary")
+					} else setLightYellow("btn-warning")
+					setLightRed("btn-secondary")
+					setLightGreen("btn-secondary")
+				}
+	
+	function changeColorGreen() {
+				if (lightGreen === "btn-success") {
+					setLightGreen("btn-secondary")
+					} else setLightGreen("btn-success")
+					setLightRed("btn-secondary")
+					setLightYellow("btn-secondary")
+				}
 };
 
 export default Home;
@@ -60,17 +87,61 @@ export default Home;
 	// const [lightYellow,setLightYellow] = useState("btn-warning")
 	// const [lightRed,setLightRed] = useState("btn-danger")
 
-	// function changeColor() {
+	// function changeColorRed() {
 	// 		if (lightRed === "btn-danger") {
 	// 			setLightRed("btn-secondary")
 	// 		} else setLightRed("btn-danger")
+	// }
 
+		// function changeColorYellow() {
 	// 		if (lightYellow === "btn-warning") {
 	// 			setLightYellow("btn-secondary")
 	// 		} else setLightYellow("btn-warning")
+		// }
 
+		// function changeColorGreen() {
 	// 		if (lightGreen === "btn-success") {
 	// 			setLightGreen("btn-secondary")
 	// 		} else setLightGreen("btn-success")
 	// 	}
 	////------------------------------------ MI SEGUNDO CODIGO SE APAGAN TODOS-------------------------------/////
+
+
+
+////------------------------------------ MI PRIMER CODIGO CON BOTONES Y BOTON SEMAFORO-------------------------------/////
+
+// 	const [lightGreen,setLightGreen] = useState("btn-success")
+// 	const [lightYellow,setLightYellow] = useState("btn-secondary")
+// 	const [lightRed,setLightRed] = useState("btn-secondary")
+
+// 	function changeColor() {
+// 		if (lightRed === "btn-danger") {
+// 			setLightRed("btn-secondary")
+// 			setLightGreen("btn-success")
+// 		} else if  (lightGreen === "btn-success"){
+// 			setLightGreen("btn-secondary")
+// 			setLightYellow("btn-warning")
+// 		} else if (lightYellow === "btn-warning"){
+// 			setLightYellow("btn-secondary")
+// 			setLightRed("btn-danger")
+// 		} else lightGreen === "btn-success"
+// 	}
+
+// 	return (
+// 		<>
+// 		<div className={"container d-flex justify-content-center bg-dark text-white"}>
+// 		</div>
+// 			<div className={"ligths container d-flex justify-content-center flex-column bg-dark"}>
+// 				<div className={"btn rounded-circle "+lightRed} onClick={changeColor}></div>
+// 				<div className={"btn rounded-circle "+lightYellow} onClick={changeColor}></div>
+// 				<div className={"btn rounded-circle "+lightGreen} onClick={changeColor}></div>
+// 			</div>
+// 			<div className="d-flex justify-content-center">
+// 			<div className="btn btn-info mt-5" onClick={changeColor}><b>SEMAFORO</b></div>
+// 		</div>
+// 		</>
+// 	);
+// };
+
+// export default Home;
+////------------------------------------ MI PRIMER CODIGO CON BOTONES Y BOTON SEMAFORO-------------------------------/////
